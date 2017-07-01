f775a8311c2b632b94113ae09d47b8333d536156
George Fraser <george@fivetran.com>
2017-07-01 11:53:23 -0700

    Limit size

--- PrestoTiming.sql
+++ PrestoTiming.sql
@@ -1,4 +1,4 @@
-select regexp_split(query, '\n')[1] as q, started, date_diff('millisecond', started, "end") as elapsed 
+select substring(regexp_split(query, '\n')[1], 1, 20) as q, started, date_diff('millisecond', started, "end") as elapsed 
 from system.runtime.queries 
 where state = 'FINISHED' 
 and source = 'presto-cli'
