6bbf2b9d7f4b4ee0453a99f32aaff3f61a420b58
George Fraser <george@fivetran.com>
2017-07-02 16:12:19 -0700

    Help BigQuery optimizer

--- query/query1.sql
+++ query/query1.sql
@@ -3,19 +3,24 @@ WITH customer_total_return
      AS (SELECT sr_customer_sk     AS ctr_customer_sk, 
                 sr_store_sk        AS ctr_store_sk, 
                 Sum(sr_return_amt) AS ctr_total_return 
-         FROM   store_returns, 
-                date_dim 
+         FROM   tpcds.store_returns, 
+                tpcds.date_dim 
          WHERE  sr_returned_date_sk = d_date_sk 
                 AND d_year = 2001 
          GROUP  BY sr_customer_sk, 
-                   sr_store_sk) 
+                   sr_store_sk),
+high_return AS (
+    SELECT ctr_store_sk, Avg(ctr_total_return) * 1.2 AS return_limit
+    FROM   customer_total_return ctr2 
+    GROUP BY ctr_store_sk
+)
 SELECT c_customer_id 
 FROM   customer_total_return ctr1, 
-       store, 
-       customer 
-WHERE  ctr1.ctr_total_return > (SELECT Avg(ctr_total_return) * 1.2 
-                                FROM   customer_total_return ctr2 
-                                WHERE  ctr1.ctr_store_sk = ctr2.ctr_store_sk) 
+       tpcds.store, 
+       tpcds.customer 
+WHERE  ctr1.ctr_total_return > (SELECT return_limit
+                                FROM high_return
+                                WHERE  ctr1.ctr_store_sk = high_return.ctr_store_sk) 
        AND s_store_sk = ctr1.ctr_store_sk 
        AND s_state = 'TN' 
        AND ctr1.ctr_customer_sk = c_customer_sk 
