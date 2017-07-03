e278ce264aa12b33c1c9f5df7b6ed318434d865b
George Fraser <george@fivetran.com>
2017-07-03 12:47:46 -0700

    Fix GROUP BY

--- query/query99.sql
+++ query/query99.sql
@@ -38,10 +38,6 @@ WHERE  d_month_seq BETWEEN 1200 AND 1200 + 11
        AND cs_warehouse_sk = w_warehouse_sk 
        AND cs_ship_mode_sk = sm_ship_mode_sk 
        AND cs_call_center_sk = cc_call_center_sk 
-GROUP  BY Substr(w_warehouse_name, 1, 20), 
-          sm_type, 
-          cc_name 
-ORDER  BY Substr(w_warehouse_name, 1, 20), 
-          sm_type, 
-          cc_name
+GROUP  BY 1, 2, 3
+ORDER  BY 1, 2, 3
 LIMIT 100; 
