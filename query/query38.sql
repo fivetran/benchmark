d22cdf5f5a38e8d869288b32f21b721670a2e30e
George Fraser <george@fivetran.com>
2017-07-02 18:00:34 -0700

    Fix for BQ, fix double-queries

--- query/query38.sql
+++ query/query38.sql
@@ -1,6 +1,6 @@
 -- query38
-SELECT Count(*) 
-FROM   (SELECT DISTINCT c_last_name, 
+WITH g1 AS (
+        SELECT DISTINCT c_last_name, 
                         c_first_name, 
                         d_date 
         FROM   store_sales, 
@@ -9,7 +9,7 @@ FROM   (SELECT DISTINCT c_last_name,
         WHERE  store_sales.ss_sold_date_sk = date_dim.d_date_sk 
                AND store_sales.ss_customer_sk = customer.c_customer_sk 
                AND d_month_seq BETWEEN 1188 AND 1188 + 11 
-        INTERSECT 
+), g2 AS (
         SELECT DISTINCT c_last_name, 
                         c_first_name, 
                         d_date 
@@ -19,7 +19,7 @@ FROM   (SELECT DISTINCT c_last_name,
         WHERE  catalog_sales.cs_sold_date_sk = date_dim.d_date_sk 
                AND catalog_sales.cs_bill_customer_sk = customer.c_customer_sk 
                AND d_month_seq BETWEEN 1188 AND 1188 + 11 
-        INTERSECT 
+), g3 AS (
         SELECT DISTINCT c_last_name, 
                         c_first_name, 
                         d_date 
@@ -28,5 +28,10 @@ FROM   (SELECT DISTINCT c_last_name,
                customer 
         WHERE  web_sales.ws_sold_date_sk = date_dim.d_date_sk 
                AND web_sales.ws_bill_customer_sk = customer.c_customer_sk 
-               AND d_month_seq BETWEEN 1188 AND 1188 + 11) hot_cust
+               AND d_month_seq BETWEEN 1188 AND 1188 + 11
+)
+SELECT Count(*) 
+FROM   g1 
+JOIN g2 ON g1.c_last_name = g2.c_last_name AND g1.c_first_name = g2.c_first_name AND g1.d_date = g2.d_date
+JOIN g3 ON g1.c_last_name = g3.c_last_name AND g1.c_first_name = g3.c_first_name AND g1.d_date = g3.d_date
 LIMIT 100; 
