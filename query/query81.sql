978856b55b54433d5a5b5b5202db94841b5c9d57
George Fraser <george@fivetran.com>
2017-07-03 12:35:02 -0700

    Help BQ query planner

--- query/query81.sql
+++ query/query81.sql
@@ -11,7 +11,12 @@ WITH customer_total_return
                 AND d_year = 1999 
                 AND cr_returning_addr_sk = ca_address_sk 
          GROUP  BY cr_returning_customer_sk, 
-                   ca_state) 
+                   ca_state),
+high_return AS (
+    SELECT ctr_state AS hr_state, Avg(ctr_total_return) * 1.2 AS hr_limit
+    FROM   customer_total_return 
+    GROUP BY ctr_state
+)
 SELECT c_customer_id, 
                c_salutation, 
                c_first_name, 
@@ -28,15 +33,15 @@ SELECT c_customer_id,
                ca_gmt_offset, 
                ca_location_type, 
                ctr_total_return 
-FROM   customer_total_return ctr1, 
+FROM   customer_total_return, 
+       high_return,
        customer_address, 
-       customer 
-WHERE  ctr1.ctr_total_return > (SELECT Avg(ctr_total_return) * 1.2 
-                                FROM   customer_total_return ctr2 
-                                WHERE  ctr1.ctr_state = ctr2.ctr_state) 
+       customer
+WHERE  ctr_state = hr_state 
+       AND ctr_customer_sk = c_customer_sk 
        AND ca_address_sk = c_current_addr_sk 
        AND ca_state = 'TX' 
-       AND ctr1.ctr_customer_sk = c_customer_sk 
+       AND ctr_total_return > hr_limit
 ORDER  BY c_customer_id, 
           c_salutation, 
           c_first_name, 
