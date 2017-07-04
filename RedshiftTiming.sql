d76114e2c2c3450b392d7621ab5da4d8d8a3b1c8
George Fraser <george@fivetran.com>
2017-07-04 13:28:36 -0700

    Fix

--- RedshiftTiming.sql
+++ RedshiftTiming.sql
@@ -1,4 +1,4 @@
-select substring(querytxt, 0, 20), starttime, datediff(milliseconds, starttime, endtime) 
+select substring(querytxt, 1, 30), starttime, datediff(milliseconds, starttime, endtime) 
 from stl_query 
 where userid = 100 
 order by starttime;
