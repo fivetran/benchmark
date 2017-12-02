d9e05de9512ac4be5a928478123f188473271c03
George Fraser <george@fivetran.com>
2017-12-02 08:57:06 -0800

    Name is random now

--- ForwardProfiler.sh
+++ ForwardProfiler.sh
@@ -1,5 +1,5 @@
 # Forward 10999 to remote worker so we can profile it
-gcloud compute ssh tpcds-w-0 \
+gcloud compute ssh $1 \
     --zone "us-central1-f" \
     --ssh-flag="-4NL" \
     --ssh-flag="10999:localhost:10999"
