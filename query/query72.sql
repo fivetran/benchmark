4b29a4be381d4f74fcf5d712f681495a233b4712
George Fraser <george@fivetran.com>
2017-12-02 10:49:54 -0800

    This cross-join makes no sense

--- query/query72.sql
+++ query/query72.sql
@@ -14,22 +14,19 @@ WITH top_items AS (
                 Count(*) total_cnt 
   FROM   catalog_sales 
         JOIN inventory 
-          ON ( cs_item_sk = inv_item_sk ) 
+          ON ( cs_item_sk = inv_item_sk and cs_sold_date_sk = inv_date_sk and cs_warehouse_sk = inv_warehouse_sk ) 
         JOIN customer_demographics 
           ON ( cs_bill_cdemo_sk = cd_demo_sk ) 
         JOIN household_demographics 
           ON ( cs_bill_hdemo_sk = hd_demo_sk ) 
         JOIN date_dim d1 
           ON ( cs_sold_date_sk = d1.d_date_sk ) 
-        JOIN date_dim d2 
-          ON ( inv_date_sk = d2.d_date_sk )
         LEFT OUTER JOIN promotion 
                       ON ( cs_promo_sk = p_promo_sk ) 
         LEFT OUTER JOIN catalog_returns 
                       ON ( cr_item_sk = cs_item_sk 
                           AND cr_order_number = cs_order_number ) 
-  WHERE  d1.d_week_seq = d2.d_week_seq 
-        AND inv_quantity_on_hand < cs_quantity 
+  WHERE inv_quantity_on_hand < cs_quantity 
         AND hd_buy_potential = '501-1000' 
         AND d1.d_year = 2002 
         AND cd_marital_status = 'M' 
