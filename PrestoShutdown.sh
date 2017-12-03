8c84ad7ec69a09779d4204db03aedcacc27bcf7f
George Fraser <george@fivetran.com>
2017-12-03 12:13:39 -0800

    Simplify

--- PrestoShutdown.sh
+++ PrestoShutdown.sh
@@ -2,18 +2,14 @@
 curl -H 'Content-Type: application/json' -X PUT -d '"SHUTTING_DOWN"' http://localhost:8080/v1/info/state
 
 HOSTNAME=\"$(hostname)\"
-STATE=$(curl http://localhost:8080/v1/info/state)
-gcloud logging write presto-shutdown "{\"hostname\":$HOSTNAME,\"state\":$STATE}" --payload-type json
-
-# Wait 5s for coordinator to acknowledge worker is shutting down
-sleep 5
 
 # Wait for all tasks to finish running
 while true; do 
+    STATE=$(curl http://localhost:8080/v1/info/state)
     TASKS=$(curl http://localhost:8080/v1/task)
     gcloud logging write presto-shutdown "{\"hostname\":$HOSTNAME,\"state\":$STATE,\"tasks\":$TASKS}" --payload-type json
+    sleep 1
     if [ $TASKS = '[]' ]; then 
         exit 0
     fi
-    sleep 1
 done
