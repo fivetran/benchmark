7bdf08b87836178d8e30c9a288e06685a5ab6194
George Fraser <george@fivetran.com>
2017-12-03 12:13:50 -0800

    Simplify

--- PrestoTiming.sql
+++ PrestoTiming.sql
@@ -1,4 +1,4 @@
-select substring(regexp_split(query, '\n')[1], 1, 20) as q, started, date_diff('millisecond', started, "end") as elapsed 
+select substring(regexp_split(query, '\n')[1], 1, 20) as q, date_diff('millisecond', started, "end") as elapsed 
 from system.runtime.queries 
 where state = 'FINISHED' 
 and source = 'presto-cli'
