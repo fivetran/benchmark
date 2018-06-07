9009204ee6d31a385681be469e492fe801573466
George Fraser <george@fivetran.com>
2018-06-06 17:03:32 -0700

    Fix upload

--- 7-ConnectPresto.sh
+++ 7-ConnectPresto.sh
@@ -2,9 +2,8 @@
 zip -r query.zip \
       Warmup.sql \
       PrestoTiming.sql \
-      ParquetDdl.sql \
-      PopulateRaptor.sql \
-      100-BenchmarkPresto.sh \
+      100-PopulatePresto.sh \
+      101-BenchmarkPresto.sh \
       query
 gcloud compute scp query.zip tpcds-presto-m:~ \
       --zone "us-central1-f"
