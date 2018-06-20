e8cbadd0b9fad727b9e8b37491c2829652bcfc09
George Fraser <george@fivetran.com>
2018-06-20 13:57:51 -0700

    Use redshift.zip

--- 7-ConnectPresto.sh
+++ 7-ConnectPresto.sh
@@ -1,11 +1,11 @@
 # Copy benchmarking files to Presto master and connect
-zip -r query.zip \
+zip -r presto.zip \
       Warmup.sql \
       PrestoTiming.sql \
       100-PopulatePresto.sh \
       101-BenchmarkPresto.sh \
       query
-gcloud compute scp query.zip tpcds-presto-m:~ \
+gcloud compute scp presto.zip tpcds-presto-m:~ \
       --zone "us-central1-f"
 gcloud compute ssh tpcds-presto-m \
       --zone "us-central1-f"
--- 8-ConnectToEc2Instance.sh
+++ 8-ConnectToEc2Instance.sh
@@ -3,12 +3,12 @@ HOST=ec2-35-166-93-125.us-west-2.compute.amazonaws.com
 PEM_FILE=~/.ssh/george.pem
 
 # Copy benchmarking files to EC2 instance
-zip -r query.zip \
+zip -r redshift.zip \
       Warmup.sql \
-      PrestoTiming.sql \
+      RedshiftTiming.sql \
       200-PopulateRedshift.sh \
       201-BenchmarkRedshift.sh \
       query
 
-scp -i ${PEM_FILE} query.zip ec2-user@${HOST}:~
+scp -i ${PEM_FILE} redshift.zip ec2-user@${HOST}:~
 ssh -i ${PEM_FILE} ec2-user@${HOST}
