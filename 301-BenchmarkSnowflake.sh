3276cf9eba2eee27ffa7a15800a85d11b54387f1
George Fraser <george@fivetran.com>
2018-06-22 13:08:10 -0700

    Profile snowflake using shell

--- 301-BenchmarkSnowflake.sh
+++ 301-BenchmarkSnowflake.sh
@@ -1,4 +1,28 @@
-# Combine all queries with cat Warmup.sql query/*.sql | pbcopy 
-# Paste into Snowflakes web editor and 'Execute all'
-# 
-# Run SnowflakeTiming.sql to get timing info
+# Run export SNOWSQL_PWD=<password> before running this script
+set -e 
+
+echo 'Warmup.sql...'
+while read line;
+do
+    snowsql -q "$line" \
+        --accountname fivetran \
+        --username tpcds_user \
+        --dbname tpcds \
+        --schemaname public \
+        --warehouse tpcds 
+done < Warmup.sql
+
+echo 'Query,Time' > RedshiftResults.csv
+for FILE in query/*.sql; 
+do
+    echo $FILE
+    /usr/bin/time -f "%e" snowsql -f $FILE \
+        --accountname fivetran \
+        --username tpcds_user \
+        --dbname tpcds \
+        --schemaname public \
+        --warehouse tpcds 
+    RUNTIME=$(cat time.txt)
+    echo "Elapsed: ${RUNTIME}s"
+    echo ${FILE},${RUNTIME} >> RedshiftResults.csv
+done
