aed699d340c3cb056ade9c98e0d7dcf48f45dfc9
George Fraser <george@fivetran.com>
2018-06-29 16:24:53 -0700

    Dead code

--- RedshiftTiming.sql
+++
@@ -1,4 +0,0 @@
-select substring(querytxt, 1, 30), datediff(milliseconds, starttime, endtime) 
-from stl_query 
-where userid = 100 
-order by starttime;
--- SnowflakeTiming.sql
+++
@@ -1,7 +0,0 @@
-select 
-    substring(query_text, 0, 20), 
-    start_time, 
-    date_part(epoch_millisecond, end_time) - date_part(epoch_millisecond, start_time) as elapsed
-from table(information_schema.query_history_by_session(session_id => current_session()::int))
-where execution_status = 'SUCCESS'
-order by start_time;
