212826b89c746d81b455f7a5586bc6923309c064
George Fraser <george@fivetran.com>
2018-07-05 14:59:06 +0530

    Run redshift a second time with enable_result_cache_for_session = off

---
+++ 203-BenchmarkRedshiftAgain.sh
@@ -0,0 +1,42 @@
+set -e 
+
+# Set HOST, PGPASSWORD, OUTPUT manually
+if [ -z "$HOST" ]; then 
+  echo "You must set HOST"
+  exit 1
+fi 
+if [ -z "$PGPASSWORD" ]; then 
+  echo "You must set PGPASSWORD"
+  exit 1
+fi 
+if [ -z "$OUTPUT" ]; then 
+  echo "You must set OUTPUT"
+  exit 1
+fi 
+
+DB=dev
+USER=tpcds_user
+
+echo 'Warmup.sql...'
+while read line;
+do
+  psql --host ${HOST} --port 5439 --user ${USER} ${DB} \
+    --echo-queries --output /dev/null \
+    --command "$line" 
+done < Warmup.sql
+
+echo 'Query,Time' > ${OUTPUT}
+for FILE in query/*.sql; 
+do
+  echo $FILE
+  sed -i -e 's/Substr(/Substring(/g' $FILE
+  echo 'set enable_result_cache_for_session = off; ' > query-${OUTPUT}.sql
+  cat $FILE >> query-${OUTPUT}.sql
+  /usr/bin/time -f "%e" psql \
+    --host ${HOST} --port 5439 --user ${USER} ${DB} \
+    --output /dev/null \
+    --file query-${OUTPUT}.sql &> time-${OUTPUT}.txt
+  RUNTIME=$(cat time-${OUTPUT}.txt)
+  echo "Elapsed: ${RUNTIME}s"
+  echo ${FILE},${RUNTIME} >> ${OUTPUT}
+done
