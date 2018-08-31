eec0619e371b6b87dd34957b9aeb16a2f3e8f3aa
Eric O'Connor <eric@fivetran.com>
2018-08-31 16:40:23 -0700

    Microsoft tool install script

---
+++ MicrosoftTools.sh
@@ -0,0 +1,5 @@
+exit 1
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo apt-get update
+sudo apt-get install mssql-tools unixodbc-dev
