f99107463d6c3f5e398864ef745e788a7af4e20b
George Fraser <george@fivetran.com>
2020-02-20 17:18:18 -0800

    Copy using rclone

--- 4-CopyToS3.sh
+++ 4-CopyToS3.sh
@@ -1,5 +1,7 @@
 # Copy CSV files to s3 for Redshift and Snowflake
 # You will need ~/.aws/credentials for this to work
 # You should run this on a gcloud instance for speed
-gsutil -m cp -r gs://fivetran-benchmark/tpcds_1000/csv s3://fivetran-benchmark/tpcds_1000/csv
+# rclone allows us to copy >5GB files using S3 multipart uploads, which gsutil does not support.
+# https://rclone.org/
+rclone copy -v gs://fivetran-benchmark/tpcds_1000_dat s3://fivetran-tpcds/tpcds_1000_dat
 
