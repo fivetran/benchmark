67143c86277bc55156b7205cab106a297c80b11f
Michael Adin Weinberg <mweinberg@joebiden.com>
2021-07-14 23:32:11 -0700

    use dbt sources queries 89

--- dbt/models/tpc_ds/query89.sql
+++ dbt/models/tpc_ds/query89.sql
@@ -12,10 +12,10 @@ FROM  (SELECT i_category,
                   partition BY i_category, i_brand, s_store_name, s_company_name 
                 ) 
                                   avg_monthly_sales 
-       FROM   item, 
-              store_sales, 
-              date_dim, 
-              store 
+       FROM   {{source('src__tpc_ds', 'item')}},
+              {{source('src__tpc_ds', 'store_sales')}},
+              {{source('src__tpc_ds', 'date_dim')}},
+              {{source('src__tpc_ds', 'store')}}
        WHERE  ss_item_sk = i_item_sk 
               AND ss_sold_date_sk = d_date_sk 
               AND ss_store_sk = s_store_sk 
